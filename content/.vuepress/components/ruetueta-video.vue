4bb4e9a14516587a3a88249a65a2ab7580ee27dc
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2019-02-05 06:18:00 +0100

    Added video-component

---
+++ content/.vuepress/components/ruetueta-video.vue
@@ -0,0 +1,45 @@
+<template>
+  <section @click="clicked = true">
+    <div class="placeholder">
+      <img v-if="!clicked" class="icon" src="../../assets/youtube.svg">
+    </div>
+    <iframe v-if="clicked" src="https://www.youtube-nocookie.com/embed/TijSQYZoRUU" frameborder="0" allowfullscreen></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {clicked: false}
+  }
+}
+</script>
+
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  background-image: url('../../assets/video-thumb.jpg');
+  background-size: cover;
+  cursor: pointer;
+}
+.placeholder {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+.icon {
+  width: 25%;
+  height: auto;
+  filter: drop-shadow(0px 0px 10px rgba(0,0,0,0.1));
+}
+iframe, .placeholder {
+  width: 100%;
+  height: 200px;
+
+  border-radius: var(--border-radius);
+}
+</style>
