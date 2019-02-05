3f3948b0c8c42739a088f610522959db2935ca9f
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2019-02-05 07:41:11 +0100

    Added nav-component

---
+++ content/.vuepress/components/ruetueta-nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <nav>
+      <router-link to="/tickets.html" class="nav-link">Tickets</router-link>
+      <router-link to="/fahrplan.html" class="nav-link">Fahrplan</router-link>
+      <router-link to="/anfahrt-unterkunft.html" class="nav-link">Anfahrt & Unterkunft</router-link>
+      <router-link to="/faq.html" class="nav-link">F.A.Q.</router-link>
+    </nav>
+  </section>
+</template>
+
+// @ToDo: rainbow colors, responsive side margin
+
+<style scoped>
+section {
+  position: sticky;
+  top: 0;
+  z-index: 20;
+}
+nav {
+  min-height: 38px;
+  max-width: 920px;
+
+  padding: 8px 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+
+  border-radius: var(--border-radius);
+  background-color: var(--white);
+  box-shadow: 0 4px 12px rgba(34, 34, 34, 0.25);
+}
+@media (max-width: 920px) {
+  nav {
+    border-radius: 0;
+  }
+}
+a {
+  padding: 2px 4px;
+
+  color: var(--black);
+  font-size: 18px;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+}
+</style>
