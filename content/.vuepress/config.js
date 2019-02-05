9456242bddbf304fea11ff3467d74ae0ae476eb5
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2019-02-05 07:42:33 +0100

    Set default locale

---
+++ content/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '42. Rüspeler Tüfteltage',
+  description: 'Das Frickelfestival im Kliemannsland. 17.-19. Mai 2019',
+  locales: {
+    // Vuepress uses the default lang-attribute 'en-US'
+    '/': {
+      lang: 'de-DE', // this will be set as the lang attribute on <html>
+    }
+  },
+  markdown: {
+    anchor: { permalink: false }
+  }
+}
