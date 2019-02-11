10bf796bb11df408a327ddef7ec050345d93cfb1
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2019-02-11 04:52:56 +0100

    Added font-display strategy

--- content/.vuepress/theme/Layout.vue
+++ content/.vuepress/theme/Layout.vue
@@ -138,6 +138,7 @@ main, .card {
   font-family: 'Space Mono';
   font-style: normal;
   font-weight: 400;
+  font-display: swap;
   src:
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
@@ -147,6 +148,7 @@ main, .card {
   font-family: 'Space Mono';
   font-style: italic;
   font-weight: 400;
+  font-display: swap;
   src:
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-italic.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-italic.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
@@ -156,6 +158,7 @@ main, .card {
   font-family: 'Space Mono';
   font-style: normal;
   font-weight: 700;
+  font-display: swap;
   src:
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-700.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-700.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
@@ -165,6 +168,7 @@ main, .card {
   font-family: 'Space Mono';
   font-style: italic;
   font-weight: 700;
+  font-display: swap;
   src:
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-700italic.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
        url('/fonts/space-mono-v4-latin/space-mono-v4-latin-700italic.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
