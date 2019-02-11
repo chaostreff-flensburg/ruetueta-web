eae2438ec2d32ffec286c05f03bbe7bee99768d5
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2019-02-11 05:06:55 +0100

    Preload self hosted fonts to reduce critical request chain

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -7,6 +7,12 @@ module.exports = {
       lang: "de-DE" // this will be set as the lang attribute on <html>
     }
   },
+  head: [
+    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-regular.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
+    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-italic.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
+    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-700.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
+    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-700italic.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
+  ],
   markdown: {
     anchor: { permalink: false },
     extendMarkdown: md => {
